6fea8d992742dab6043fd41b14b70778107bf51b
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-18 17:53:04 +0400

    Configure system packages, services, and user settings for NixOS

---
+++ nix/services.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }: {
+  services = {
+    tailscale = {
+      enable = true;
+    };
+    openssh = {
+        enable = true;               # Enable the OpenSSH daemon
+        settings = {
+        PermitRootLogin = "no";    # Disable root login
+        PasswordAuthentication = false;  # Disable password authentication
+        };
+        
+        # Optionally, configure authorized keys here
+        openFirewall = true;  # Alternative way to open SSH port
+    };
+  };
+
+  virtualisation = {
+    docker = {
+      enable = true;
+    };
+  };
+}
